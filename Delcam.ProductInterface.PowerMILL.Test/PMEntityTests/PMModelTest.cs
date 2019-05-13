2dfda04b6d945a5a93f5bdcaaa5a99670ef61c5f
Feuerbowle <Feuerbowle@googlemail.com>
2019-05-13 11:01:12 +0200

    Update PMModelTest.cs

--- Delcam.ProductInterface.PowerMILL.Test/PMEntityTests/PMModelTest.cs
+++ Delcam.ProductInterface.PowerMILL.Test/PMEntityTests/PMModelTest.cs
@@ -176,17 +176,36 @@ namespace Autodesk.ProductInterface.PowerMILLTest.PMEntityTests
         [Test]
         public void RotateTest()
         {
+            _powerMill.Execute("DEACTIVATE WORKPLANE");
             // #1
-            // Rotate by 90° in X-Axis
-            _defaultModel.Rotate(Axes.X, 90);
+            // Rotate by 180° in X-Axis
+            var bxBefore = _defaultModel.BoundingBox;
+            _defaultModel.Rotate(Axes.X, 180);
+            var bxAfter = _defaultModel.BoundingBox;
+            Assert.AreEqual(bxBefore.MinY * -1, bxAfter.MaxY);
+            Assert.AreEqual(bxBefore.MaxY * -1, bxAfter.MinY);
+            Assert.AreEqual(bxBefore.MinZ * -1, bxAfter.MaxZ);
+            Assert.AreEqual(bxBefore.MaxZ * -1, bxAfter.MinZ);
 
             // #2
-            // Rotate by 90° in Y-Axis
-            _defaultModel.Rotate(Axes.Y, 90);
+            // Rotate by 180° in Y-Axis
+            bxBefore = _defaultModel.BoundingBox;
+            _defaultModel.Rotate(Axes.Y, 180);
+            bxAfter = _defaultModel.BoundingBox;
+            Assert.AreEqual(bxBefore.MinX * -1, bxAfter.MaxX);
+            Assert.AreEqual(bxBefore.MaxX * -1, bxAfter.MinX);
+            Assert.AreEqual(bxBefore.MinZ * -1, bxAfter.MaxZ);
+            Assert.AreEqual(bxBefore.MaxZ * -1, bxAfter.MinZ);
 
             // #3
-            // Rotate by 90° in Z-Axis
-            _defaultModel.Rotate(Axes.Z, 90);
+            // Rotate by 180° in Z-Axis
+            bxBefore = _defaultModel.BoundingBox;
+            _defaultModel.Rotate(Axes.Z, 180);
+            bxAfter = _defaultModel.BoundingBox;
+            Assert.AreEqual(bxBefore.MinX * -1, bxAfter.MaxX);
+            Assert.AreEqual(bxBefore.MaxX * -1, bxAfter.MinX);
+            Assert.AreEqual(bxBefore.MinY * -1, bxAfter.MaxY);
+            Assert.AreEqual(bxBefore.MaxY * -1, bxAfter.MinY);
         }
 
         [Test]
