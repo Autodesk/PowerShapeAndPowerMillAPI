4960d1a35ee28aa1a8127a0f1bab1176ad57ca18
George Kerwood <george.kerwood@autodesk.com>
2024-07-11 09:20:11 +0200

    Added empty test class and required files

---
+++ Delcam.ProductInterface.PowerSHAPE.Test/DatabaseEntity Tests/Entity Tests/Solid Tests/SolidPrimitiveTest.cs
@@ -0,0 +1,76 @@
+﻿// **********************************************************************
+// *         © COPYRIGHT 2024 Autodesk, Inc.All Rights Reserved         *
+// *                                                                    *
+// *  Use of this software is subject to the terms of the Autodesk      *
+// *  license agreement provided at the time of installation            *
+// *  or download, or which otherwise accompanies this software         *
+// *  in either electronic or hard copy form.                           *
+// **********************************************************************
+
+using Autodesk.ProductInterface.PowerSHAPE;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Autodesk.ProductInterface.PowerSHAPETest
+{
+    /// <summary>
+    /// This is a test class for SolidPrimitiveTest and is intended
+    /// to contain all SolidPrimitiveTest Unit Tests
+    /// </summary>
+    [TestFixture]
+    public class SolidPrimitiveTest: EntityTest<PSSolidPrimitive>
+    {
+        /// <summary>
+        /// A test for Origin
+        /// </summary>
+        [Test]
+        public void OriginTest()
+        {
+            //Automation powerSHAPE = null; // TODO: Initialize to an appropriate value
+            //Primitive target = new Primitive(powerSHAPE); // TODO: Initialize to an appropriate value
+            //Point actual;
+            //actual = target.Origin;
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        /// A test for XAXis
+        /// </summary>
+        [Test]
+        public void XAXisTest()
+        {
+            //Automation powerSHAPE = null; // TODO: Initialize to an appropriate value
+            //Primitive target = new Primitive(powerSHAPE); // TODO: Initialize to an appropriate value
+            //Vector actual;
+            //actual = target.XAXis;
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        /// A test for YAxis
+        /// </summary>
+        [Test]
+        public void YAxisTest()
+        {
+            //Automation powerSHAPE = null; // TODO: Initialize to an appropriate value
+            //Primitive target = new Primitive(powerSHAPE); // TODO: Initialize to an appropriate value
+            //Vector actual;
+            //actual = target.YAxis;
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        /// A test for ZXais
+        /// </summary>
+        [Test]
+        public void ZXaisTest()
+        {
+            //Automation powerSHAPE = null; // TODO: Initialize to an appropriate value
+            //Primitive target = new Primitive(powerSHAPE); // TODO: Initialize to an appropriate value
+            //Vector actual;
+            //actual = target.ZXais;
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
